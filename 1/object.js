031df16e46da810720c30ce4b034b1730fc5df72
SHEKOOFEHSHOJAI <shekoofehshojai.78@gmail.com>
2022-07-10 01:52:47 -0700

    sh

---
+++ 1/object.js
@@ -0,0 +1,25 @@
+var useone={
+    email:'shekoofehshojai',
+    name:'shekoofeh',
+    login(){
+
+        console.log(this.email,'has login in')
+
+    }
+    ,
+    logout(){
+
+     console.log(this.email,'has log out')
+    }
+}
+useone.name='yoshi';
+//can poya
+var prop='name';
+  console.log("prop...",useone[prop]);       
+useone['name']='shojai'
+console.log(useone.name);
+console.log(this);
+
+
+useone.age=25;
+console.log(useone);
