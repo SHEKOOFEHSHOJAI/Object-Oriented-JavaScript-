ba04724638a21d27adfb64b7a47393065682708b
SHEKOOFEHSHOJAI <shekoofehshojai.78@gmail.com>
2022-07-16 21:34:17 -0700

    sh

--- learn-react/my-app/src/Home.js
+++ learn-react/my-app/src/Home.js
@@ -1,12 +1,24 @@
 const  Home= () => {
+    
     const handelClick=()=>{
         console.log('hello,ninja');
     }
+
+
+     const handelClickAgain=(name)=>{
+        console.log('hello',name);
+    }
+    
     return (  
 
         <div className="home">
             <h1>Home page</h1>
-            <button onClick={handelClick}>Click Me </button>
+           <button onClick={handelClick}>Click Me </button>
+           {/* for  */}
+            <button onClick={()=>{
+               handelClickAgain('mario')
+
+            }}></button>
         </div>
     );
 }
