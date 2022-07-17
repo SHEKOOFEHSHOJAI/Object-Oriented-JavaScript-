d5a36ca849da6ce570dba01897cc6858dafdf491
SHEKOOFEHSHOJAI <shekoofehshojai.78@gmail.com>
2022-07-16 20:52:53 -0700

    shekoofeh

--- learn-react/my-app/src/App.js
+++ learn-react/my-app/src/App.js
@@ -1,23 +1,29 @@
 
-import './App.css';
+
 import React from "react";
+import Navbar from "./Navbar";
+import Home from './Home';
+
 function App() {
-  const title='welcome eo the new blog'
-  const likes=50;
-  const link="http://www.google.com";
+  const title='welcome to the new blog'
+  // const likes=50;
+  // const link="http://www.google.com";
  
   return (
     <div className="App">
+      <Navbar/>
       <div className="content">
         <h1>{title}</h1>
-        <p>liked {likes} times</p>
+        {/* <p>liked {likes} times</p> */}
 
          {/* <p>{10}</p> */}
 
-         <p>{"hello ninja"}</p>
+         {/* <p>{"hello ninja"}</p>
          <p>{[1,2,3,4,5]}</p>
          <p>{Math.random()*20}</p>
-         <a href={link}>Google Site</a>
+         <a href={link}>Google Site</a> */}
+         <Home/>
+
       </div>
 
      
--- learn-react/my-app/src/Home.js
+++ learn-react/my-app/src/Home.js
@@ -1,9 +1,12 @@
 const  Home= () => {
+    const handelClick=()=>{
+        console.log('hello,ninja');
+    }
     return (  
 
         <div className="home">
             <h1>Home page</h1>
-
+            <button onClick={handelClick}>Click Me </button>
         </div>
     );
 }
