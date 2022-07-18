a94f68abfe34e17e068566604c71cb3955e7bdd6
SHEKOOFEHSHOJAI <shekoofehshojai.78@gmail.com>
2022-07-18 04:41:43 -0700

    shekoofeh

--- learn-react/my-app/src/Home.js
+++ learn-react/my-app/src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import BlogList from "./BlogList";
 
 const  Home= () => {
@@ -27,15 +27,17 @@ const  Home= () => {
 
 
     const handelDelete = (id) => {
-        // if == false filter
-        // if !=true
+        // if == false filter  =>remove 
+        // if !=true=> we want keep in arry
         const newBlog=blogs.filter((blog)=>blog.id !==id )
         setBlogs(newBlog)
       console.log(blogs);
     };
 
-
-    
+//render for each change data or refresh broweser 
+    useEffect(()=>{
+        console.log('USEEFECT RUN');
+    })
     return (
       <div className="home">
         {/* <h1>Home page</h1> */}
