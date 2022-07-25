488ecd281734782cc7587d482c559194c49a4dc9
SHEKOOFEHSHOJAI <shekoofehshojai.78@gmail.com>
2022-07-25 06:23:15 -0700

    custom hook

--- learn-react/my-app/src/BlogDetail.js
+++ learn-react/my-app/src/BlogDetail.js
@@ -1,11 +1,28 @@
 import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
 
 //Route Parameters
 const  BlogDetails= () => {
     const {id}=useParams()
+    const {
+      data: blog,
+      error,
+      isPending,
+    } = useFetch(" http://localhost:8000/blogs/" + id);
+    
     return ( 
         <div className="blog-detail">
-            <h2>Blog Details-{id}</h2>
+            {isPending && <div>loading...!!!</div>}
+            {error && <div>{error}</div>}
+            {blog &&(
+                <article>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                    <div>{blog.body}</div>
+                </article>
+            )}
+            {/* <h2>Blog Details-{id}</h2> */}
+           
         </div>
      );
 }
