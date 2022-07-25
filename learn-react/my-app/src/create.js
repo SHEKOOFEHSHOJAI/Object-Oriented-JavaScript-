0b27ec41fca03ec56ab46b39a8e278b0783966b3
SHEKOOFEHSHOJAI <shekoofehshojai.78@gmail.com>
2022-07-25 08:06:21 -0700

    sh

--- learn-react/my-app/src/create.js
+++ learn-react/my-app/src/create.js
@@ -1,9 +1,48 @@
+import { useState } from "react";
+
 const Create = () => {
-    return ( 
-        <div className="create">
-            <h2>Add new Blog</h2>
-        </div>
-     );
+    const [title,setTitle]=useState('')
+    const [body, setBody] = useState("");
+    const [auter, setAuter] = useState("");
+    const handeleSubmit=(e)=>{
+        //don't refresh website
+     e.preventDefault()
+     const blog={title,body,auter};
+     console.log(blog);
+        
+    }
+    return (
+      <div className="create">
+        <h2>Add new Blog</h2>
+        <form onSubmit={handeleSubmit}>
+          <label>Blog title</label>
+          <input
+            type="text"
+            required
+            onChange={(e) => setTitle(e.target.value)}
+          />
+
+          <label>Blog body</label>
+          <textarea
+            value={body}
+            onChange={(e) =>setBody( e.target.value)}
+            required
+          ></textarea>
+
+          <label>Blog auter</label>
+          <select
+          value={auter}
+          onChange={(e)=>setAuter(e.target.value)}
+          >
+            <option value="mario">mario</option>
+            <option value="yoshi">yoshi</option>
+          </select>
+          <button>Add Blog</button>
+          <p>{title}</p>
+          <p>{auter}</p>
+        </form>
+      </div>
+    );
 }
  
 export default Create;
