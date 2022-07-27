cdc39ab0de65759bc7413724fe9ed25a590e4094
SHEKOOFEHSHOJAI <shekoofehshojai.78@gmail.com>
2022-07-27 12:57:08 -0700

    sh

--- 1/lo.js
+++ 1/lo.js
@@ -18,3 +18,39 @@ console.log(email.lastIndexOf('h'));
 
 console.log(email.slice(0,4));
 console.log(email.substr(0, 4));
+
+console.log(email.replace('sh','m'));
+
+// math operators +,-,*,/,**,%
+// order oprators -B=braket I=tavan m=*
+let result=5*(10-3)**2
+
+// template string
+console.log(email+result+"  "+"i am oket"+result);
+
+// Template Strings for many varible
+let templtaString=`the blog called ${email}  by s`;
+
+// creating html teml]plate
+let html=`<h2>${email}</h2>`;
+let ninja=['shayan','ryn','chun-li'];
+// exit from arr and between add (,)
+let join=ninja.join(',')
+console.log(join)
+// concat two arr
+let concat=ninja.concat(['shyda','crystal'])
+let push=ninja.push('a am pushhh')
+ninja.pop()
+console.log(ninja);
+
+
+// methods can return boolean
+let includes=email.includes('sh')
+console.log(includes);
+let age=23;
+console.log(age==23);
+// in javascript chracter samll biger than 
+let name='shayn';
+console.log(name>'Shayn');
+//s next form c
+console.log(name > "crystal");
